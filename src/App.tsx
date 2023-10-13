77665caf04b93c6af9b4564e8940be809dd8fc7b
Anthony Medina <anthony.john.medina@gmail.com>
2023-10-13 16:05:19 -0400

    built pause and resume functions using clearTimer and getDeadTime Functions, intaking timeStrings set to state

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,120 @@
 import './global.css'
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { SimpleGrid, Box, Button } from '@chakra-ui/react'
 
-function App() {
-  const [currentBoxCount, setCurrentBoxCount] = useState<number>(1)
+const boosterBox = {
+  title: 'Super Awesome Spoils Tournament',
+  game: 'The Spoils',
+  initialTime: '01:02:30',
+  timeAtPause: ''
+}
 
-  const addBox = (): void => {
-    setCurrentBoxCount(currentBoxCount + 1)
+const App = () => {
+  const Ref = useRef(null)
+  const [timer, setTimer] = useState('00:00:00')
+  const [isPaused, setIsPaused] = useState(false)
+
+  const getTimeRemaining = (e) => {
+    const total = Date.parse(e) - Date.parse(new Date())
+    const seconds = Math.floor((total / 1000) % 60)
+    const minutes = Math.floor((total / 1000 / 60) % 60)
+    const hours = Math.floor((total / 1000 / 60 / 60) % 24)
+    console.log(total, hours, minutes, seconds)
+    console.log('This is the timer', timer)
+    return {
+      total,
+      hours,
+      minutes,
+      seconds
+    }
   }
 
-  const renderBoxes = (currentBoxCount: number) => {
-    return [...Array(currentBoxCount)].map((currentBox: number) => (
-      <Box bg="tomato" h="96.5vh">
-        <Button colorScheme="blue" onClick={addBox}>
-          Add Box
-        </Button>
-      </Box>
-    ))
+  const startTimer = (e) => {
+    let { total, hours, minutes, seconds } = getTimeRemaining(e)
+    if (total >= 0) {
+      setTimer(
+        (hours > 9 ? hours : '0' + hours) +
+          ':' +
+          (minutes > 9 ? minutes : '0' + minutes) +
+          ':' +
+          (seconds > 9 ? seconds : '0' + seconds)
+      )
+    }
+  }
+
+  const clearTimer = (e) => {
+    if (!boosterBox.timeAtPause || timer == '00:00:00') {
+      setTimer(boosterBox.initialTime)
+    } else {
+      setTimer(boosterBox.timeAtPause)
+    }
+
+    if (Ref.current) clearInterval(Ref.current)
+    const id = setInterval(() => {
+      startTimer(e)
+    }, 1000)
+    Ref.current = id
+  }
+
+  const getDeadTime = (timeString: string) => {
+    let deadline = new Date()
+    console.log('This is the deadline before modification > ', deadline)
+
+    console.log('This is the incoming timeString > ', timeString)
+    const arrayFromTimeString = timeString.split(':')
+    console.log(
+      'This is the outgoing arrayFromTimeString > ',
+      arrayFromTimeString
+    )
+    const hours = parseInt(arrayFromTimeString[0])
+    console.log('This is hours ', hours)
+    const minutes = parseInt(arrayFromTimeString[1])
+    console.log('This is minutes ', minutes)
+    const seconds = parseInt(arrayFromTimeString[2])
+    console.log('This is seconds ', seconds)
+
+    deadline.setSeconds(deadline.getSeconds() + seconds)
+    deadline.setMinutes(deadline.getMinutes() + minutes)
+    deadline.setHours(deadline.getHours() + hours)
+    console.log('This is the deadline > ', deadline)
+    return deadline
+  }
+
+  useEffect(() => {
+    clearTimer(getDeadTime(boosterBox.initialTime))
+  }, [])
+
+  const onClickReset = () => {
+    clearTimer(getDeadTime(boosterBox.initialTime))
+  }
+
+  const onClickPause = () => {
+    boosterBox.timeAtPause = timer
+    console.log(Boolean(boosterBox.timeAtPause))
+    console.log('This is the time at pause', boosterBox.timeAtPause)
+    clearInterval(Ref.current)
+    setIsPaused(!isPaused)
+  }
+
+  const onClickResume = () => {
+    clearTimer(getDeadTime(boosterBox.timeAtPause))
+    setIsPaused(!isPaused)
+  }
+
+  const renderMoreControls = () => {
+    return !isPaused ? (
+      <button onClick={onClickPause}>Pause</button>
+    ) : (
+      <button onClick={onClickResume}>Start</button>
+    )
   }
 
   return (
-    <>
-      <SimpleGrid columns={3} gap="40px" p={4}>
-        {renderBoxes(currentBoxCount)}
-      </SimpleGrid>
-    </>
+    <div className="App">
+      <h2>{timer}</h2>
+      <button onClick={onClickReset}>Reset</button>
+      {renderMoreControls()}
+    </div>
   )
 }
 
