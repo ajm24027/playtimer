7c1f90392ee299bc69b1fe8a3cc3cd5ac50eb13b
Anthony Medina <anthony.john.medina@gmail.com>
2024-03-18 22:05:00 -0400

    added alarm on timer expire

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,14 +1,35 @@
+import {
+  Center,
+  VStack,
+  HStack,
+  Button,
+  Box,
+  useDisclosure
+} from '@chakra-ui/react'
+import { CloseIcon } from '@chakra-ui/icons'
+import { IncomingTimerProps, PhaseNavProps } from '../../types/app-types'
+import { GameKey, timerStyles } from './timerStyles'
+import { useTimer } from './useTimer'
+import { Modal } from '../Modal'
+import TimingPhase from '../ModalSteps/TimingPhase'
+import alarm from '../Timer/electronic-alarm-clock-151927.mp3'
 
-import { Center, VStack, HStack, Button, Box, useDisclosure } from "@chakra-ui/react";
-import { CloseIcon } from "@chakra-ui/icons";
-import { IncomingTimerProps, PhaseNavProps } from "../../types/app-types";
-import { GameKey, timerStyles } from "./timerStyles";
-import { useTimer } from "./useTimer";
-import { Modal } from "../Modal";
-import TimingPhase from "../ModalSteps/TimingPhase";
-
-const TimerComp = ({ name, initialTime, game, terminateTimer }: IncomingTimerProps) => {
-  const { value, isExpired, isPaused, onPause, onReset, onResume, changeTime, currentOptions } = useTimer(initialTime)
+const TimerComp = ({
+  name,
+  initialTime,
+  game,
+  terminateTimer
+}: IncomingTimerProps) => {
+  const {
+    value,
+    isExpired,
+    isPaused,
+    onPause,
+    onReset,
+    onResume,
+    changeTime,
+    currentOptions
+  } = useTimer(initialTime)
   const { isOpen, onOpen, onClose } = useDisclosure()
 
   const onOpenChangeTime = () => {
@@ -16,29 +37,43 @@ const TimerComp = ({ name, initialTime, game, terminateTimer }: IncomingTimerPro
     onOpen()
   }
 
-  const onChangeTime: PhaseNavProps['onClickNext'] = value => {
-    changeTime(value.initialTime!);
+  const onChangeTime: PhaseNavProps['onClickNext'] = (value) => {
+    changeTime(value.initialTime!)
     onClose()
   }
 
   const renderControls = () => {
-    const resetButton = (<Button colorScheme="orange" w="100%" onClick={onReset}>Reset</Button>)
+    const resetButton = (
+      <Button colorScheme="orange" w="100%" onClick={onReset}>
+        Reset
+      </Button>
+    )
 
     if (isExpired) {
       return resetButton
     }
-    return (<HStack spacing={8} w="100%">
-      {resetButton}
-      {!isPaused
-        ? <Button colorScheme="yellow" w="100%" onClick={onPause}>Pause</Button>
-        : <Button colorScheme="green" w="100%" onClick={onResume}>Start</Button>}
-      <Button colorScheme="cyan" w="100%" onClick={onOpenChangeTime}>Change Time</Button>
-    </HStack>)
-  };
+    return (
+      <HStack spacing={4} w="100%">
+        {resetButton}
+        {!isPaused ? (
+          <Button colorScheme="yellow" w="100%" onClick={onPause}>
+            Pause
+          </Button>
+        ) : (
+          <Button colorScheme="green" w="100%" onClick={onResume}>
+            Start
+          </Button>
+        )}
+        <Button colorScheme="cyan" w="100%" onClick={onOpenChangeTime}>
+          Change
+        </Button>
+      </HStack>
+    )
+  }
 
   const renderTimer = (game: GameKey) => {
-    const gameData = timerStyles[game];
-    const { backgroundImage, borderColor, boxShadow } = gameData;
+    const gameData = timerStyles[game]
+    const { backgroundImage, borderColor, boxShadow } = gameData
     return (
       <Box
         h="100%"
@@ -47,14 +82,14 @@ const TimerComp = ({ name, initialTime, game, terminateTimer }: IncomingTimerPro
         backgroundPosition="center"
         borderRadius="16px"
         boxShadow={
-          isExpired || isPaused ? "none" : `6px 6px 32px 11px ${boxShadow}`
+          isExpired || isPaused ? 'none' : `6px 6px 32px 11px ${boxShadow}`
         }
         border={`4px solid ${isExpired ? '#787878' : borderColor}`}
       >
         <Box
           display="flex"
           flexDirection="column"
-          backdropFilter={isExpired ? "grayscale(100%)" : "none"}
+          backdropFilter={isExpired ? 'grayscale(100%)' : 'none'}
           w="100%"
           h="100%"
         >
@@ -84,20 +119,33 @@ const TimerComp = ({ name, initialTime, game, terminateTimer }: IncomingTimerPro
             </VStack>
           </Center>
         </Box>
+        {isExpired && (
+          <audio style={{ display: 'none' }} controls autoPlay>
+            <source src={alarm} type="audio/mp3" />
+          </audio>
+        )}
       </Box>
-    );
-  };
-
-  const renderChangeTime = () => {
-    return <Modal isOpen={isOpen} title="Change Time" onClose={onClose}>
-      <TimingPhase onClickBack={onClose} onClickNext={onChangeTime} initialValue={currentOptions} />
-    </Modal>
+    )
   }
 
-  return <>
-    {renderTimer(game)}
-    {renderChangeTime()}
-  </>
-};
+  const renderChangeTime = () => {
+    return (
+      <Modal isOpen={isOpen} title="Change Time" onClose={onClose}>
+        <TimingPhase
+          onClickBack={onClose}
+          onClickNext={onChangeTime}
+          initialValue={currentOptions}
+        />
+      </Modal>
+    )
+  }
 
-export default TimerComp;
+  return (
+    <>
+      {renderTimer(game)}
+      {renderChangeTime()}
+    </>
+  )
+}
+
+export default TimerComp
